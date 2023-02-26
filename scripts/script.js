dae11b6259f0ab0d8571bb837e6023d7e0ea0e66
Vitor Gabriel <victoropo64@outlook.com>
2023-02-26 12:38:44 -0400

    imc function

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+// dados do usuario
+
+let altura = document.querySelector('input#altura')
+let peso = document.querySelector('input#peso')
+let resultado_imc = document.querySelector('div#result')
+
+// dados da tabela
+
+let abaixo_peso = document.querySelector('tr#magro')
+let peso_normal = document.querySelector('tr#nomal')
+let sobre_peso = document.querySelector('tr#sobrepeso')
+let obesidade = document.querySelector('tr#obesidade')
+let obesiade_morbida = document.querySelector('tr#obesidade_morbida')
+
+
+function verificador() {
+    if (altura.value.length === 0 || peso.value.length === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function situacao(indice) {
+    if (indice < 18.5) {
+        abaixo_peso.style.background = '#01e2e269'
+    } else if (indice >= 18.5 && indice)
+}
+
+function calculo() {
+    if (verificador()) {
+        alert('Preencha todos os dados antes de calcular o IMC')
+    } else {
+        let imc = (Number(peso.value)) / (Number(altura.value) ** 2)
+        resultado_imc.innerHTML = ` ${imc.toFixed(2)} `
+        situacao(imc)
+    }
+}
+
+function limpar() {
+    altura.value = ''
+    peso.value = ''
+    resultado_imc.innerHTML = ''
+}
+
